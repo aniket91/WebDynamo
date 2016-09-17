6499b0da90004a74e82701855c62a2e2afa8d90f
athakur <anikthak@in.ibm.com>
2016-09-17 21:22:51 +0530

    Adding log4j configuration

---
+++ src/com/osfg/controllers/Monitor.java
@@ -0,0 +1,22 @@
+package com.osfg.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class Monitor {
+	
+	Logger logger = LoggerFactory.getLogger(Monitor.class);
+	
+	@RequestMapping(value="/test.htm",method=RequestMethod.GET)
+	public String test() {
+		logger.debug("Entered Controller : {} and method : {}", "Monitor", "test");
+		return "pages/index.html";
+	}
+
+}
